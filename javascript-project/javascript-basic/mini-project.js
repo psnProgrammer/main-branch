97aba71f37135ae74ad89fbd1a892bc862fcdd91
Ong <personal.programmingg@gmail.com>
2020-12-20 19:09:14 +0700

    add btn to change and reset bg

--- javascript-project/javascript-basic/mini-project.js
+++ javascript-project/javascript-basic/mini-project.js
@@ -1,5 +1,16 @@
-function changeBackground(){
-    document.body.style.backgroundColor = "red";
+function resetBg(){
+    document.body.style.backgroundColor = "white";
 }
 
-changeBackground();
+function randomBg(){
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    
+}
+
+
