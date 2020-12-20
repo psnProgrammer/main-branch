4a54a4c72e7f87014a4ee09b900d64689b455d78
Ong <personal.programmingg@gmail.com>
2020-12-20 19:00:09 +0700

    add function - change bg

---
+++ javascript-project/javascript-basic/mini-project.js
@@ -0,0 +1,5 @@
+function changeBackground(){
+    document.body.style.backgroundColor = "red";
+}
+
+changeBackground();
--- javascript-project/javascript-basic/script.js
+++ javascript-project/javascript-basic/script.js
@@ -4,6 +4,23 @@ console.log("External Script ! ");
 
 document.getElementById("text").innerHTML = ("Text from document.getElementById innerHTML");
 
-document.write("Write Document");
+document.write("Write Document\n");
 
-window.alert("Window Alert Box");
+window.alert("Window Alert Box"); 
+
+// variables
+
+var price = 1;
+var price2 = 2;
+var sum = price + price2;
+
+console.log("\nsum = " + sum);
+
+
+// functions
+
+function myFunction(value1,value2){
+    return value1+value2
+}
+
+console.log(myFunction(3,5))
