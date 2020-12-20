11c99597cc7ed1ea29ab9e8cb408283113aa7a18
Ong <personal.programmingg@gmail.com>
2020-12-20 18:08:56 +0700

    basic output

---
+++ javascript-project/javascript-basic/script.js
@@ -0,0 +1,9 @@
+// output 
+
+console.log("External Script ! ");
+
+document.getElementById("text").innerHTML = ("Text from document.getElementById innerHTML");
+
+document.write("Write Document");
+
+window.alert("Window Alert Box");
