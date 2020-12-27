479a4c183e19336f36da95d2b0fdc0361eb4bf7b
Ong <personal.programmingg@gmail.com>
2020-12-27 15:07:09 +0700

    hotfix and add switch date

--- javascript-project/javascript-basic/mini-project.js
+++ javascript-project/javascript-basic/mini-project.js
@@ -1,8 +1,11 @@
 function resetBg(){
+    document.querySelector("#resetbgbtn").disabled = true;
     document.body.style.backgroundColor = "white";
 }
 
 function randomBg(){
+    document.querySelector("#resetbgbtn").disabled = false;
+
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
@@ -16,21 +19,21 @@ function randomBg(){
 function convertDegreeC2F(celcius){
     var c = celcius;
     var f = c * 9 / 5 + 32;
-    var result = c + '\xB0C is ' + f + '\xB0F';  // celcius and fahrenheit code
+    var result = c + "\xB0C is " + f + "\xB0F";  // celcius and fahrenheit code
 
     console.log(result);
 }
 
-console.log(convertDegreeC2F(30));
+convertDegreeC2F(30);
 
 function convertDegreeF2C(fahrenheit){
     var f = fahrenheit;
     var c = (f - 32) * 5 / 9;
-    var result = f + '\xB0F is ' + c + '\xB0C';  // celcius and fahrenheit code
+    var result = f + "\xB0F is " + c + "\xB0C";  // celcius and fahrenheit code
 
     console.log(result);
 }
-console.log(convertDegreeF2C(30));
+convertDegreeF2C(30);
 
 function showCar(){
     var car = {
@@ -51,16 +54,72 @@ function showCar(){
 showCar();
 
 function displayDateTime(){
-    document.getElementById('datetime').innerHTML = Date();
+    document.getElementById("datetime").innerHTML = Date();
+    hidden = false
+    document.getElementById("datetime").style.visibility = "visible";
+    document.getElementById("invbtn").style.visibility = "visible";
+    document.querySelector("#invbtn").innerText = "Hide time";
+    document.querySelector("#timebtn").innerText = "Update Time";
+
 }
 
 
 function displayArrayAsString(){
     var vendor = ["Honda","Kawasaki","Suzuki"];
-    // document.getElementById('vendor').innerHTML = vendor.toString();
-    document.getElementById('vendor').innerHTML = vendor.join(' - ')
-
+    // document.getElementById("vendor").innerHTML = vendor.toString();
+    console.log("Display Arr as String: " + vendor.join(" - "))
 
 }
 displayArrayAsString();
 
+function switchDate(){
+    switch(new Date().getDay()){
+        case 0:
+            console.log("Today is sunday")
+            break;
+        case 1:
+            console.log("Today is monday")
+            break;
+        case 2:
+            console.log("Today is tuesday")
+            break;
+        case 3:
+            console.log("Today is wednesday")
+            break;
+        case 4:
+            console.log("Today is thursday")
+            break;
+        case 5:
+            console.log("Today is friday")
+            break;
+        case 6:
+            console.log("Today is saturday")
+            break;
+
+        
+    }
+}
+
+switchDate()
+
+hidden = false;
+
+function invisibleMe(){
+    
+    hidden = !hidden;
+    if(hidden){
+        document.getElementById("datetime").style.visibility = "hidden";
+        // document.querySelector("#timebtn").innerText = "Show time";
+        document.querySelector("#timebtn").disabled = true;
+
+        document.querySelector("#invbtn").innerText = "Show time";
+
+    }else{
+        document.getElementById("datetime").style.visibility = "visible";
+        document.querySelector("#invbtn").innerText = "Hide time";
+        document.querySelector("#timebtn").disabled = false;
+    }
+
+}
+
+document.getElementById("console").innerHTML = "**Other result in dev tools: console";
